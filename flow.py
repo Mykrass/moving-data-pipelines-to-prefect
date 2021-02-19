7ed594a7bb8d1caa179df5249dc04ad5ebf3fb46
Igor Krasovskiy <i.krasovskiy@gmail.com>
2021-02-19 23:11:47 +0200

    Add files.

---
+++ flow.py
@@ -0,0 +1,16 @@
+from  prefect import Flow, Task, task
+from prefect.environments.storage import Docker
+from prefect.triggers import always_run
+
+@task
+def flow_pieces():
+    import python.do_all_the_machine_learning
+
+with Flow('shiny new flow',
+          storage=Docker(base_image='spookyimage-shiny',
+              local_image=True)) as f:
+    flow_pieces()
+
+
+    ## run flow and print logs
+    f.reqister(project_name= 'monster-project')
---
+++ python/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='monstermash', # Repalace with your own username
+    version='0.0.1',
+    author='Mykras',
+    author_email='mykras@me.com',
+    description='My custom ML monsters in a package',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
