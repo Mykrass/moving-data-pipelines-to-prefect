5d7d5d300d1852aaef6f9ebbcff8c088b1ecd8d4
Igor Krasovskiy <i.krasovskiy@gmail.com>
2021-02-20 19:35:32 +0200

    Refactoring.

--- python/manipulation.py
+++ python/manipulation.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import sqlite3
+import requests
+from prefect import task
+import prefect
+
+@task
+def extract(url, file_number):
+    logger = prefect.context.get('logger')
+    logger.info('Downloading data...')
+    response = requests.get(url)
+    filename = f'data-{file_number}.json'
+    with open(filename, 'wt') as f:
+        f.write(response.text)
+    return filename
 
 def transfor_features(filename):
     df = pd.read_json(filename)
@@ -13,7 +26,6 @@ def transfor_features(filename):
     return df[['Normalized Age', 'Normalized Balance']]
 
 
-#
 # create table models(id integer primary key autoincrement, name varchar(255) not null
 def save_to_db(database, filename):
     with open(filename, 'rb') as f:
