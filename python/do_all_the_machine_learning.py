10d9bb68dadb3896b6f1cfd480b7f380c8b4bdce
Igor Krasovskiy <i.krasovskiy@gmail.com>
2021-04-12 15:17:39 +0300

    Refactoring.

--- python/do_all_the_machine_learning.py
+++ python/do_all_the_machine_learning.py
@@ -5,7 +5,7 @@ from manipulation import transform_features, save_to_db
 from machine_learning import its_a_linear_regression
 
 input_file = os.path.abspath(sys.argv[1])
-output_file = input_file.replace('.json','.joblib')
+output_file = input_file.replace('.json', '.joblib')
 dt = transform_features(sys.argv[1])
 its_a_linear_regression(dt, output_file)
 DBNAME = os.environ['DB_NAME']
